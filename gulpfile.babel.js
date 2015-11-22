df4bf725d1dcb63e9fa5d5447a5eab7839f1d5fc
Jack Scott <jack.rob.scott@gmail.com>
2015-11-22 19:54:24 +0800

    initialise repo with es6 support and linting etc.

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+const gulp = require('gulp');
+const del = require('del');
+const babel = require('gulp-babel');
+
+const config = {
+  src: 'src',
+  dest: 'generators',
+}
+
+gulp.task('clean', () => del(config.dest));
+
+gulp.task('compile', gulp.series('clean', () => {
+  return gulp.src(path.join(config.src, '**'), {
+      dot: true
+    })
+    .pipe(babel({
+      presets: ['es2015'],
+      ignore: [
+        '**/templates/**',
+        '**/*.json',
+      ],
+    }))
+    .pipe(gulp.dest(config.dest));
+}));
